f04edce5320b7d1a69a6e90bc210240e8f18695a
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-02 19:19:13 +0300

    HTTP easy-hand service

--- ANGULAR/project/src/app/core/core.module.ts
+++ ANGULAR/project/src/app/core/core.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
 })
 export class CoreModule {}
