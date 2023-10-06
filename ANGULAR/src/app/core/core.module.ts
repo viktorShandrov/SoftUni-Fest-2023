34bfcb3c6b7982fb201b2761b6b6a49754e0c907
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-10-06 23:15:11 +0300

    update

--- ANGULAR/src/app/core/core.module.ts
+++ ANGULAR/src/app/core/core.module.ts
@@ -6,9 +6,11 @@ import { ApiInterceptor } from './interceptors/api.interceptor';
 import { AsideComponent } from './components/top/aside/aside.component';
 import { SearchComponent } from './components/top/search/search.component';
 import { TopComponent } from './components/top/top.component';
+import { UsersComponent } from './components/bottom/users/users.component';
 
 @NgModule({
-  declarations: [AsideComponent, SearchComponent, TopComponent],
+  declarations: [AsideComponent, SearchComponent, TopComponent, UsersComponent],
   imports: [CommonModule],
+  exports: [AsideComponent, SearchComponent, TopComponent, UsersComponent],
 })
 export class CoreModule {}
