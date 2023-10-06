c968fef0e02a1b0e8939623d5bc7a37948268052
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-06 22:45:51 +0300

    delete message

--- REST-API/src/controllers/messageController.js
+++ REST-API/src/controllers/messageController.js
@@ -6,17 +6,27 @@ const messageManager = require("../managers/messageManager");
 const router = express.Router();
 
 
-router.post("/createMessage",isAuth,async (req,res)=>{
+router.post("/createMessage",isAuth,async (req,res)=> {
+    try {
+        const {messageText, roomId} = req.body
+        const {_id} = req.user
+        const newMessage = await messageManager.createMessage(messageText, _id)
+        await roomManager.addMessageToRoom(roomId, newMessage._id)
+        res.status(200).end()
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+router.post("/deleteMessage",isAuth,async (req,res)=>{
     try{
-        const {messageText,roomId} = req.body
+        const {roomId,messageId} = req.body
         const {_id}= req.user
-        const newMessage = await messageManager.createMessage(messageText,_id)
-        await roomManager.addMessageToRoom(roomId,newMessage._id)
+        await messageManager.deleteMessage(roomId,messageId,_id)
         res.status(200).end()
     }catch (error){
-    res.status(400).json({message:error.message})
-}
+        res.status(400).json({message:error.message})
+    }
 
 })
 
-module.exports = router;
+module.exports = router
--- REST-API/src/managers/messageManager.js
+++ REST-API/src/managers/messageManager.js
@@ -1,10 +1,24 @@
 const utils = require("../utils/utils")
 const messageModel = require("../models/messageModel")
 const bcrypt = require("bcrypt")
+const roomModel = require("../models/roomModel");
 
 
 
 exports.createMessage=async (message,owner)=>{
     return messageModel.create({text:message,owner})
     
+}
+exports.deleteMessage=async (roomId,messageId,userId)=>{
+    const room = await roomModel.findById(roomId)
+    if(!room) throw new Error("No such room")
+
+    const message = await messageModel.findById(messageId)
+    if(!message) throw new Error("No such message")
+
+    if(!message.owner.equals(userId)) throw new Error("You are not the owner of this message")
+
+    const messageIndex = room.messages.indexOf(messageId)
+    room.messages.splice(messageIndex,1)
+    return room.save()
 }
