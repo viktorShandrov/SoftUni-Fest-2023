070d74b1450ab0ccbbc7582b54c94c629ad1cae1
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-10-06 21:23:05 +0300

    update

--- ANGULAR/src/app/app.module.ts
+++ ANGULAR/src/app/app.module.ts
@@ -7,10 +7,20 @@ import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core/core.module";
 import {FeaturedModule} from "./featured/featured.module";
 import {FormsModule} from "@angular/forms";
+import { TopComponent } from './top/top.component';
+import { SearchComponent } from './search/search.component';
+import { AsideComponent } from './aside/aside.component';
+import { BottomComponent } from './bottom/bottom.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopComponent,
+    SearchComponent,
+    AsideComponent,
+    BottomComponent,
+    UsersComponent,
 
   ],
   imports: [
