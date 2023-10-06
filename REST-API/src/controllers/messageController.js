c52001428695d2588f1402167069119bdb6211b5
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-06 22:04:29 +0300

    rest

--- REST-API/src/controllers/messageController.js
+++ REST-API/src/controllers/messageController.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const {isAuth} = require("../utils/auth");
 const roomManager = require("../managers/roomManager");
+const messageManager = require("../managers/messageManager");
 
 const router = express.Router();
 
 
 router.post("/createMessage",isAuth,async (req,res)=>{
-    const {roomId} = req.body
-    await roomManager.createRoom(roomName)
-    await 
+    const {messageText,roomId} = req.body
+    const {_id}= req.user
+    const newMessage = await messageManager.createMessage(messageText,_id)
+    await roomManager.addMessageToRoom(roomId,newMessage._id)
 })
 
 module.exports = router;
--- REST-API/src/managers/messageManager.js
+++ REST-API/src/managers/messageManager.js
@@ -1,13 +1,10 @@
 const utils = require("../utils/utils")
-const roomModel = require("../models/roomModel")
+const messageModel = require("../models/messageModel")
 const bcrypt = require("bcrypt")
 
 
-exports.createRoom=async (roomName)=>{
-    return roomModel.create({name:roomName})
-}
-exports.createMessage=async (roomId)=>{
-    const room = roomModel.findById(roomId)
-    if(!room) throw new Error("No such room")
+
+exports.createMessage=async (message,owner)=>{
+    return messageModel.create({text:message,owner})
     
 }
