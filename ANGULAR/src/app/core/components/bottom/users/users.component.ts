95f2059285b7f60410714029d32c5f3c0e31c93f
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-10-07 13:54:45 +0300

    update

--- ANGULAR/src/app/core/components/bottom/users/users.component.ts
+++ ANGULAR/src/app/core/components/bottom/users/users.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent {
-
+  constructor(private elementRef: ElementRef, private renderer2: Renderer2) {
+    renderer2.setStyle(elementRef.nativeElement, 'flex', 1);
+  }
 }
