9cfe30ed44e0d86f0f1ed76d374333fecc20fdc8
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-11 20:32:28 +0300

    .

--- ANGULAR/src/app/core/directives/aside-menu-btn.directive.ts
+++ ANGULAR/src/app/core/directives/aside-menu-btn.directive.ts
@@ -1,4 +1,5 @@
 import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {HtmlElementsService} from "../../shared/services/html-elements.service";
 
 @Directive({
   selector: '[appAsideMenuBtn]'
@@ -7,12 +8,15 @@ export class AsideMenuBtnDirective {
   @Input("menuToShow") menuToShow!:ElementRef
   constructor(
     private element:ElementRef,
+    private HtmlElementsService:HtmlElementsService,
     private Renderer2:Renderer2,
   ) {
   }
 
     @HostListener("click")
     onClick(){
+      this.Renderer2.setStyle(this.HtmlElementsService.elements['offcanvas'],"display","none")
+      
       this.Renderer2.setStyle(this.menuToShow.nativeElement,"display","flex")
     }
 
