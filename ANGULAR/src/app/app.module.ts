fc3cc0f7028eac28841158a04211f512218991c5
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-27 13:43:21 +0300

    vvvvvv

--- ANGULAR/src/app/app.module.ts
+++ ANGULAR/src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { FeaturedModule } from './featured/featured.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
+=======
+import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core/core.module";
+import {FeaturedModule} from "./featured/featured.module";
+import {FormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {ApiInterceptor} from "./core/interceptors/api.interceptor";
+import {TokenInterceptor} from "./core/interceptors/token.interceptor";
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,7 +34,23 @@ import { ToastrModule } from 'ngx-toastr';
     FeaturedModule,
     FormsModule,
   ],
+<<<<<<< Updated upstream
   providers: [],
   bootstrap: [AppComponent],
+=======
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true, // Set multi to true to allow multiple interceptors
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true, // Set multi to true to allow multiple interceptors
+    },
+  ],
+  bootstrap: [AppComponent]
+>>>>>>> Stashed changes
 })
 export class AppModule {}
