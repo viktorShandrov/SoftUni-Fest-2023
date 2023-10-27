d675d23db3f3927eeff27494db0c4c0d79b7bacc
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-10-27 15:38:21 +0300

    update

--- ANGULAR/src/app/featured/private/catalog/catalog.component.ts
+++ ANGULAR/src/app/featured/private/catalog/catalog.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.css'],
 })
 export class CatalogComponent {
-
+  businessmen = [
+    {
+      company: 'sada',
+      name: 'asdad',
+    },
+    {
+      company: 'sada',
+      name: 'asdad',
+    },
+    {
+      company: 'sada',
+      name: 'asdad',
+    },
+    {
+      company: 'sada',
+      name: 'asdad',
+    },
+    {
+      company: 'sada',
+      name: 'asdad',
+    },
+  ];
 }
