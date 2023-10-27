1cb5a66538983213b057f356926768645339573b
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-27 13:44:50 +0300

    ccccccc

--- ANGULAR/src/app/app.module.ts
+++ ANGULAR/src/app/app.module.ts
@@ -9,8 +9,10 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { FeaturedModule } from './featured/featured.module';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
+import {ApiInterceptor} from "./core/interceptors/api.interceptor";
+import {TokenInterceptor} from "./core/interceptors/token.interceptor";
 
 
 
