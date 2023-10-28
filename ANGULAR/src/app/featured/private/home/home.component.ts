29c837fc1ed81f3c1379fdbde1a32629c8e12e07
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-28 16:57:17 +0300

    reviews have aniomation now on scrolling

--- ANGULAR/src/app/featured/private/home/home.component.ts
+++ ANGULAR/src/app/featured/private/home/home.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit{
+
+  constructor() {
+
+  }
+  ngAfterViewInit(){
+    const elementsToAnimate:any = document.querySelectorAll('.reviewC');
+
+    const offset = 100; // Set the scroll offset when the animation triggers
+
+    function checkScroll() {
+
+      for (const elementToAnimateElement of elementsToAnimate) {
+        const elementPosition = elementToAnimateElement!.getBoundingClientRect().top;
+        if (elementPosition - window.innerHeight < offset) {
+          elementToAnimateElement!.classList.add('animated');
+        }
+      }
+
+    }
+
+    window.addEventListener('scroll', checkScroll);
+  }
+}
