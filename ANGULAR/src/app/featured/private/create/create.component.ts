3d6ab34c36262e1207af2b9a04f3459628a4adcc
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-10-29 10:01:11 +0200

    update

--- ANGULAR/src/app/featured/private/create/create.component.ts
+++ ANGULAR/src/app/featured/private/create/create.component.ts
@@ -1,50 +1,52 @@
 import { Component } from '@angular/core';
-import {CreateService} from "../../services/create.service";
-import {ToastrService} from "ngx-toastr";
-import {ActivatedRoute, Router} from "@angular/router";
+import { CreateService } from '../../services/create.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent {
-  editMode= false
+  editMode = false;
+
   constructor(
-    public CreateService:CreateService,
-    private ToastrService:ToastrService,
-    private Router:Router,
+    public CreateService: CreateService,
+    private ToastrService: ToastrService,
+    private Router: Router,
     private route: ActivatedRoute
   ) {
-    this.route.queryParams.subscribe((params:any) => {
-    if (params.edit === 'true') {
-      this.editMode = true
+    this.route.queryParams.subscribe((params: any) => {
+      if (params.edit === 'true') {
+        this.editMode = true;
+      }
+    });
+  }
+
+  editOffer(offer: any) {
+    this.CreateService.editOffer(offer);
+  }
+
+  submit(form: any) {
+    if (form.invalid) {
+      this.ToastrService.error('Please fill in all required fields.', 'Error');
+      return;
     }
-  });
-  }
-
-
-  editOffer(offer:any){
-    this.CreateService.editOffer(offer)
-  }
-
-
-  submit(form:any){
-    if(form.invalid) return
     this.CreateService.createOffer(
       form.value.name,
       form.value.description,
       form.value.price,
-      form.value.type,
+      form.value.type
     ).subscribe(
-      (res:any)=>{
-        this.Router.navigate(["/offerDetails",res.offer._id])
-        this.ToastrService.success("Successfully created", "Happy message")
-
+      (res: any) => {
+        this.Router.navigate(['/offerDetails', res.offer._id]);
+        this.ToastrService.success('Successfully created', 'Happy message');
+        form.reset();
       },
       (error) => {
-        this.ToastrService.error(error.error.message, "Error")
+        this.ToastrService.error(error.error.message, 'Error');
       }
-    )
+    );
   }
 }
